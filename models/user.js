cf100cfa6256692b6eb97bc1a07e3147d8649b87
Akul <akulwritess@gmail.com>
2024-01-28 15:23:39 +0530

    user schema changes

--- models/user.js
+++ models/user.js
@@ -1,14 +1,43 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  userId: { type: Number, unique: true, autoIncrement: true },
-  name: { type: String, required: true },
-  dairy_name: { type: String },
-  father_name: { type: String },
-  phone: { type: String, unique: true },
-  address: { type: String },
-  pan: { type: String },
-  pincode: { type: Number }
+var userSchema = new mongoose.Schema({
+  user_id: { 
+    type: Number, 
+    unique: true, 
+    autoIncrement: true
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  dairy_name: { 
+    type: String 
+  },
+  father_name: { 
+    type: String 
+  },
+  phone: { 
+    type: String, 
+    unique: true 
+  },
+  address: { 
+    type: String 
+  },
+  pan: { 
+    type: String 
+  },
+  pincode: { 
+    type: Number 
+  },
+  user_type: { 
+    type: String 
+  },
+  customers: { 
+    type: Array 
+  },
+  sub_customers: { 
+    type: Array 
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
