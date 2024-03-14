aae067d8efbc8c755c2f40e536e13c176a1d998e
Kanishk Pareek <knshkpareek@gmail.com>
2024-03-15 00:38:56 +0530

    last commit

--- routes/cart_route.js
+++ routes/cart_route.js
@@ -7,4 +7,5 @@ const cart_controller=require('../controllers/cart_controller.js')
 cart_route.post('/add_cart', cart_controller.addCart);
 cart_route.post('/remove_cart', cart_controller.removeCart);
 cart_route.get('/get_cart', cart_controller.getCart);
+
 module.exports=cart_route;
