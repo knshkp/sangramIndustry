615cce4233c38b357341ac4f90c324d39a233445
Kanishk Pareek <knshkpareek@gmail.com>
2024-04-17 22:52:42 +0530

    changes

--- services/rate_services.js
+++ services/rate_services.js
@@ -12,7 +12,49 @@ const addRate=async(data)=> {
 const getRateDetails=async(phone)=> {
     const RateData = await Rate.find({ phone : phone });
     if(RateData.length===0){
-        return [6.0,6.5,7.0,7.5];
+        return [		{
+			"type": "fatsnf",
+			"rate": "6.5",
+		},
+        {
+			"type": "fat",
+			"rate": "6.5"
+		},
+        {
+
+			"type": "fatclr",
+			"rate": "6.5"
+		},
+        {
+
+			"type": "fatclr",
+			"rate": "6.0"
+		},{
+
+			"type": "fatsnf",
+			"rate": "6.02"
+		},
+        {
+
+			"type": "fat",
+			"rate": "6.02"
+		},
+        {
+
+			"type": "fatclr",
+			"rate": "6.04"
+		},
+        {
+
+			"type": "fatsnf",
+			"rate": "6.08"
+		},
+        {
+
+			"type": "fat",
+			"rate": "7.0"
+		}
+    ];
     }
     return RateData
 }
