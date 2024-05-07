d56ebde5c7ada13747a9b83220adffedc93b85e5
Kanishk Pareek <knshkpareek@gmail.com>
2024-05-07 10:46:01 +0530

    login vendor added

--- routes/vendor.js
+++ routes/vendor.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const VendorControllers = require('../controllers/vendor')
 
 router.post('/addVendor', VendorControllers.addVendor)
-router.post('/addVendor', VendorControllers.loginVendor)
+router.post('/loginVendor', VendorControllers.loginVendor)
 
 router.get(`/getVendor`, VendorControllers.getVendorDetails)
 router.get(`/removeVendor`, VendorControllers.removeVendor)
